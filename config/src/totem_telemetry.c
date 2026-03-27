3737df96d790d2bda733b7b983267b64ce1cf7d6
UROUS <urous3814@gmail.com>
2026-03-27 19:21:06 +0900

    Add BLE telemetry for key tracker

--- config/src/totem_telemetry.c
+++ config/src/totem_telemetry.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/uuid.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/uart.h>
 #include <zephyr/init.h>
@@ -7,6 +9,7 @@
 #include <zephyr/logging/log.h>
 
 #include <zmk/battery.h>
+#include <zmk/ble.h>
 #include <zmk/event_manager.h>
 #include <zmk/keymap.h>
 
@@ -20,7 +23,27 @@ LOG_MODULE_REGISTER(totem_telemetry, CONFIG_ZMK_LOG_LEVEL);
 #define TELEMETRY_UART_NODE DT_CHOSEN(zmk_totem_telemetry_uart)
 BUILD_ASSERT(DT_NODE_EXISTS(TELEMETRY_UART_NODE), "Totem telemetry UART chosen node is missing");
 
+#define TOTEM_TELEMETRY_BLE_SERVICE_UUID_VAL                                                   \
+    BT_UUID_128_ENCODE(0x54204e5b, 0xe1db, 0x4400, 0x8906, 0x8148001ddf99)
+#define TOTEM_TELEMETRY_BLE_CHAR_UUID_VAL                                                      \
+    BT_UUID_128_ENCODE(0xe70f015f, 0xd378, 0x4aa6, 0x97d0, 0x498c3fc6bde8)
+
+#define TOTEM_TELEMETRY_BLE_VERSION 1U
+#define TOTEM_TELEMETRY_BLE_UNKNOWN_LEVEL 0xFFU
+
 static const struct device *const telemetry_uart = DEVICE_DT_GET(TELEMETRY_UART_NODE);
+
+BUILD_ASSERT(CONFIG_ZMK_SPLIT_BLE_CENTRAL_PERIPHERALS <= 8,
+             "Totem BLE telemetry supports up to 8 split peripherals");
+
+struct totem_telemetry_ble_payload {
+    uint8_t version;
+    uint8_t active_layer_index;
+    uint8_t local_battery;
+    uint8_t peripheral_count;
+    uint8_t peripheral_connected_mask;
+    uint8_t peripheral_battery[CONFIG_ZMK_SPLIT_BLE_CENTRAL_PERIPHERALS];
+};
 
 struct totem_telemetry_state {
     uint8_t active_layer_index;
@@ -35,6 +58,61 @@ static struct totem_telemetry_state telemetry_state = {
 };
 
 static struct k_work_delayable telemetry_emit_work;
+static struct totem_telemetry_ble_payload telemetry_ble_payload = {
+    .version = TOTEM_TELEMETRY_BLE_VERSION,
+    .peripheral_count = CONFIG_ZMK_SPLIT_BLE_CENTRAL_PERIPHERALS,
+};
+static bool telemetry_ble_notify_enabled;
+
+static void telemetry_schedule_emit(k_timeout_t delay);
+
+static void telemetry_refresh_state(void) {
+    telemetry_state.active_layer_index = zmk_keymap_highest_layer_active();
+    telemetry_state.local_battery = zmk_battery_state_of_charge();
+}
+
+static void telemetry_fill_ble_payload(void) {
+    telemetry_ble_payload.version = TOTEM_TELEMETRY_BLE_VERSION;
+    telemetry_ble_payload.active_layer_index = telemetry_state.active_layer_index;
+    telemetry_ble_payload.local_battery = telemetry_state.local_battery;
+    telemetry_ble_payload.peripheral_count = CONFIG_ZMK_SPLIT_BLE_CENTRAL_PERIPHERALS;
+    telemetry_ble_payload.peripheral_connected_mask = 0;
+
+    for (int i = 0; i < CONFIG_ZMK_SPLIT_BLE_CENTRAL_PERIPHERALS; i++) {
+        if (telemetry_state.peripheral_connected[i]) {
+            telemetry_ble_payload.peripheral_connected_mask |= (1U << i);
+        }
+
+        telemetry_ble_payload.peripheral_battery[i] = telemetry_state.peripheral_battery_known[i]
+                                                           ? telemetry_state.peripheral_battery[i]
+                                                           : TOTEM_TELEMETRY_BLE_UNKNOWN_LEVEL;
+    }
+}
+
+static ssize_t telemetry_read_ble_payload(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+                                          void *buf, uint16_t len, uint16_t offset) {
+    telemetry_refresh_state();
+    telemetry_fill_ble_payload();
+
+    return bt_gatt_attr_read(conn, attr, buf, len, offset, &telemetry_ble_payload,
+                             sizeof(telemetry_ble_payload));
+}
+
+static void telemetry_ble_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value) {
+    ARG_UNUSED(attr);
+
+    telemetry_ble_notify_enabled = (value == BT_GATT_CCC_NOTIFY);
+    telemetry_schedule_emit(K_NO_WAIT);
+}
+
+BT_GATT_SERVICE_DEFINE(
+    totem_telemetry_svc,
+    BT_GATT_PRIMARY_SERVICE(BT_UUID_DECLARE_128(TOTEM_TELEMETRY_BLE_SERVICE_UUID_VAL)),
+    BT_GATT_CHARACTERISTIC(BT_UUID_DECLARE_128(TOTEM_TELEMETRY_BLE_CHAR_UUID_VAL),
+                           BT_GATT_CHRC_READ | BT_GATT_CHRC_NOTIFY, BT_GATT_PERM_READ_ENCRYPT,
+                           telemetry_read_ble_payload, NULL, &telemetry_ble_payload),
+    BT_GATT_CCC(telemetry_ble_ccc_cfg_changed,
+                BT_GATT_PERM_READ_ENCRYPT | BT_GATT_PERM_WRITE_ENCRYPT));
 
 static bool telemetry_uart_ready(void) {
     if (!device_is_ready(telemetry_uart)) {
@@ -124,11 +202,21 @@ static void telemetry_write_snapshot(void) {
 static void telemetry_emit_work_handler(struct k_work *work) {
     ARG_UNUSED(work);
 
-    telemetry_state.active_layer_index = zmk_keymap_highest_layer_active();
-    telemetry_state.local_battery = zmk_battery_state_of_charge();
+    telemetry_refresh_state();
 
     if (telemetry_uart_ready()) {
         telemetry_write_snapshot();
+    }
+
+    if (telemetry_ble_notify_enabled && zmk_ble_active_profile_is_connected()) {
+        telemetry_fill_ble_payload();
+
+        int err =
+            bt_gatt_notify(NULL, &totem_telemetry_svc.attrs[1], &telemetry_ble_payload,
+                           sizeof(telemetry_ble_payload));
+        if (err != 0 && err != -ENOTCONN) {
+            LOG_WRN("Failed to notify BLE telemetry (%d)", err);
+        }
     }
 
     k_work_reschedule(&telemetry_emit_work, K_MSEC(CONFIG_TOTEM_TELEMETRY_INTERVAL_MS));
@@ -190,11 +278,10 @@ ZMK_SUBSCRIPTION(totem_telemetry, zmk_usb_conn_state_changed);
 static int totem_telemetry_init(void) {
     if (!device_is_ready(telemetry_uart)) {
         LOG_WRN("Telemetry UART device is not ready");
-        return -ENODEV;
     }
 
-    telemetry_state.active_layer_index = zmk_keymap_highest_layer_active();
-    telemetry_state.local_battery = zmk_battery_state_of_charge();
+    telemetry_refresh_state();
+    telemetry_fill_ble_payload();
 
     k_work_init_delayable(&telemetry_emit_work, telemetry_emit_work_handler);
     telemetry_schedule_emit(K_MSEC(1000));
