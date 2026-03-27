1e11305121a4e2617a4cc63d1ee81605e4c23597
UROUS <urous3814@gmail.com>
2026-03-27 19:04:25 +0900

    Add Totem dongle telemetry stream

---
+++ config/src/totem_telemetry.c
@@ -0,0 +1,205 @@
+#include <stdio.h>
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+#include <zmk/battery.h>
+#include <zmk/event_manager.h>
+#include <zmk/keymap.h>
+
+#include <zmk/events/battery_state_changed.h>
+#include <zmk/events/layer_state_changed.h>
+#include <zmk/events/usb_conn_state_changed.h>
+#include <zmk/events/split_central_status_changed.h>
+
+LOG_MODULE_REGISTER(totem_telemetry, CONFIG_ZMK_LOG_LEVEL);
+
+#define TELEMETRY_UART_NODE DT_CHOSEN(zmk_totem_telemetry_uart)
+BUILD_ASSERT(DT_NODE_EXISTS(TELEMETRY_UART_NODE), "Totem telemetry UART chosen node is missing");
+
+static const struct device *const telemetry_uart = DEVICE_DT_GET(TELEMETRY_UART_NODE);
+
+struct totem_telemetry_state {
+    uint8_t active_layer_index;
+    uint8_t local_battery;
+    uint8_t peripheral_battery[CONFIG_ZMK_SPLIT_BLE_CENTRAL_PERIPHERALS];
+    bool peripheral_battery_known[CONFIG_ZMK_SPLIT_BLE_CENTRAL_PERIPHERALS];
+    bool peripheral_connected[CONFIG_ZMK_SPLIT_BLE_CENTRAL_PERIPHERALS];
+};
+
+static struct totem_telemetry_state telemetry_state = {
+    .local_battery = 0,
+};
+
+static struct k_work_delayable telemetry_emit_work;
+
+static bool telemetry_uart_ready(void) {
+    if (!device_is_ready(telemetry_uart)) {
+        return false;
+    }
+
+    uint32_t dtr = 0;
+    if (uart_line_ctrl_get(telemetry_uart, UART_LINE_CTRL_DTR, &dtr) != 0) {
+        return false;
+    }
+
+    return dtr != 0;
+}
+
+static void telemetry_write_string(const char *str) {
+    for (size_t i = 0; str[i] != '\0'; i++) {
+        uart_poll_out(telemetry_uart, str[i]);
+    }
+}
+
+static void telemetry_write_json_string(const char *str) {
+    uart_poll_out(telemetry_uart, '"');
+
+    for (size_t i = 0; str[i] != '\0'; i++) {
+        switch (str[i]) {
+        case '\\':
+        case '"':
+            uart_poll_out(telemetry_uart, '\\');
+            uart_poll_out(telemetry_uart, str[i]);
+            break;
+        case '\n':
+            telemetry_write_string("\\n");
+            break;
+        case '\r':
+            telemetry_write_string("\\r");
+            break;
+        case '\t':
+            telemetry_write_string("\\t");
+            break;
+        default:
+            uart_poll_out(telemetry_uart, str[i]);
+            break;
+        }
+    }
+
+    uart_poll_out(telemetry_uart, '"');
+}
+
+static void telemetry_write_snapshot(void) {
+    char num_buf[16];
+    const char *layer_name =
+        zmk_keymap_layer_name(zmk_keymap_layer_index_to_id(telemetry_state.active_layer_index));
+
+    telemetry_write_string("{\"type\":\"snapshot\",\"layer\":{\"index\":");
+    snprintf(num_buf, sizeof(num_buf), "%u", telemetry_state.active_layer_index);
+    telemetry_write_string(num_buf);
+    telemetry_write_string(",\"name\":");
+    telemetry_write_json_string(layer_name != NULL ? layer_name : "");
+    telemetry_write_string("},\"battery\":{\"local\":");
+    snprintf(num_buf, sizeof(num_buf), "%u", telemetry_state.local_battery);
+    telemetry_write_string(num_buf);
+    telemetry_write_string(",\"peripherals\":[");
+
+    for (int i = 0; i < CONFIG_ZMK_SPLIT_BLE_CENTRAL_PERIPHERALS; i++) {
+        if (i > 0) {
+            telemetry_write_string(",");
+        }
+
+        telemetry_write_string("{\"slot\":");
+        snprintf(num_buf, sizeof(num_buf), "%d", i);
+        telemetry_write_string(num_buf);
+        telemetry_write_string(",\"level\":");
+        if (telemetry_state.peripheral_battery_known[i]) {
+            snprintf(num_buf, sizeof(num_buf), "%u", telemetry_state.peripheral_battery[i]);
+            telemetry_write_string(num_buf);
+        } else {
+            telemetry_write_string("-1");
+        }
+        telemetry_write_string(",\"connected\":");
+        telemetry_write_string(telemetry_state.peripheral_connected[i] ? "true" : "false");
+        telemetry_write_string("}");
+    }
+
+    telemetry_write_string("]}}\n");
+}
+
+static void telemetry_emit_work_handler(struct k_work *work) {
+    ARG_UNUSED(work);
+
+    telemetry_state.active_layer_index = zmk_keymap_highest_layer_active();
+    telemetry_state.local_battery = zmk_battery_state_of_charge();
+
+    if (telemetry_uart_ready()) {
+        telemetry_write_snapshot();
+    }
+
+    k_work_reschedule(&telemetry_emit_work, K_MSEC(CONFIG_TOTEM_TELEMETRY_INTERVAL_MS));
+}
+
+static void telemetry_schedule_emit(k_timeout_t delay) {
+    k_work_reschedule(&telemetry_emit_work, delay);
+}
+
+static int totem_telemetry_listener(const zmk_event_t *eh) {
+    const struct zmk_layer_state_changed *layer_ev = as_zmk_layer_state_changed(eh);
+    if (layer_ev != NULL) {
+        telemetry_state.active_layer_index = zmk_keymap_highest_layer_active();
+        telemetry_schedule_emit(K_NO_WAIT);
+        return ZMK_EV_EVENT_BUBBLE;
+    }
+
+    const struct zmk_battery_state_changed *local_battery_ev = as_zmk_battery_state_changed(eh);
+    if (local_battery_ev != NULL) {
+        telemetry_state.local_battery = local_battery_ev->state_of_charge;
+        telemetry_schedule_emit(K_NO_WAIT);
+        return ZMK_EV_EVENT_BUBBLE;
+    }
+
+    const struct zmk_peripheral_battery_state_changed *peripheral_battery_ev =
+        as_zmk_peripheral_battery_state_changed(eh);
+    if (peripheral_battery_ev != NULL &&
+        peripheral_battery_ev->source < CONFIG_ZMK_SPLIT_BLE_CENTRAL_PERIPHERALS) {
+        telemetry_state.peripheral_battery[peripheral_battery_ev->source] =
+            peripheral_battery_ev->state_of_charge;
+        telemetry_state.peripheral_battery_known[peripheral_battery_ev->source] = true;
+        telemetry_schedule_emit(K_NO_WAIT);
+        return ZMK_EV_EVENT_BUBBLE;
+    }
+
+    const struct zmk_split_central_status_changed *split_status_ev =
+        as_zmk_split_central_status_changed(eh);
+    if (split_status_ev != NULL && split_status_ev->slot < CONFIG_ZMK_SPLIT_BLE_CENTRAL_PERIPHERALS) {
+        telemetry_state.peripheral_connected[split_status_ev->slot] = split_status_ev->connected;
+        telemetry_schedule_emit(K_NO_WAIT);
+        return ZMK_EV_EVENT_BUBBLE;
+    }
+
+    if (as_zmk_usb_conn_state_changed(eh) != NULL) {
+        telemetry_schedule_emit(K_MSEC(50));
+        return ZMK_EV_EVENT_BUBBLE;
+    }
+
+    return ZMK_EV_EVENT_BUBBLE;
+}
+
+ZMK_LISTENER(totem_telemetry, totem_telemetry_listener);
+ZMK_SUBSCRIPTION(totem_telemetry, zmk_layer_state_changed);
+ZMK_SUBSCRIPTION(totem_telemetry, zmk_battery_state_changed);
+ZMK_SUBSCRIPTION(totem_telemetry, zmk_peripheral_battery_state_changed);
+ZMK_SUBSCRIPTION(totem_telemetry, zmk_split_central_status_changed);
+ZMK_SUBSCRIPTION(totem_telemetry, zmk_usb_conn_state_changed);
+
+static int totem_telemetry_init(void) {
+    if (!device_is_ready(telemetry_uart)) {
+        LOG_WRN("Telemetry UART device is not ready");
+        return -ENODEV;
+    }
+
+    telemetry_state.active_layer_index = zmk_keymap_highest_layer_active();
+    telemetry_state.local_battery = zmk_battery_state_of_charge();
+
+    k_work_init_delayable(&telemetry_emit_work, telemetry_emit_work_handler);
+    telemetry_schedule_emit(K_MSEC(1000));
+
+    return 0;
+}
+
+SYS_INIT(totem_telemetry_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
